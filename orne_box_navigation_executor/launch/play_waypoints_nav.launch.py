384c29a0d23b27d7a208cf15cdaf4f46efc291af
TakumiIshiguro <aquarius1496@gmail.com>
2024-11-20 02:20:24 +0900

    ✨feat: add mapserver for costmap

--- orne_box_navigation_executor/launch/play_waypoints_nav.launch.py
+++ orne_box_navigation_executor/launch/play_waypoints_nav.launch.py
@@ -14,10 +14,11 @@ def generate_launch_description():
     run_file_dir = get_package_share_directory('waypoint_manager2')
     config_dir = os.path.join(nav_dir, 'config')
 
-    map_pass = 'tsukuba2024'
+    # map_pass = 'tsukuba2024'
     # map_pass = 'cit_3f_map'
+    map_pass = 'tsudanuma_all'
 
-    map_dir = LaunchConfiguration(
+    map_data = LaunchConfiguration(
         'map',
         default=os.path.join(
             config_dir,
@@ -26,8 +27,8 @@ def generate_launch_description():
         )
     )
 
-    mask_yaml_file = LaunchConfiguration(
-        'mask',
+    costmap_data = LaunchConfiguration(
+        'costmap',
         default=os.path.join(
             config_dir,
             'maps',
@@ -48,7 +49,7 @@ def generate_launch_description():
     rviz_config_dir = os.path.join(
         config_dir,
         'rviz',
-        'nav2_default_view2.rviz'
+        'nav2_default_view.rviz'
     )
 
     lifecycle_nodes = ['filter_mask_server', 'costmap_filter_info_server']
@@ -83,55 +84,60 @@ def generate_launch_description():
             default_value=os.path.join(config_dir, 'params', 'nav2_params.yaml'),
             description='Full path to the ROS 2 parameters file to use'
         ),
-        DeclareLaunchArgument(
-            'mask',
-            default_value = mask_yaml_file,
-            description='Full path to filter mask yaml file to load'
-        ),
-        Node(
-            package='nav2_lifecycle_manager',
-            executable='lifecycle_manager',
-            name='lifecycle_manager_costmap_filters',
-            namespace=namespace,
-            output='screen',
-            emulate_tty=True,  # https://github.com/ros2/launch/issues/188
-            parameters=[{'use_sim_time': use_sim_time},
-                        {'autostart': autostart},
-                        {'node_names': lifecycle_nodes}]
-        ),
-        Node(
-            package='nav2_map_server',
-            executable='map_server',
-            name='filter_mask_server',
-            namespace=namespace,
-            output='screen',
-            emulate_tty=True,  # https://github.com/ros2/launch/issues/188
-            parameters=[RewrittenYaml(
-                        source_file=params_file,
-                        root_key=namespace,
-                        param_rewrites={'use_sim_time': use_sim_time,
-                                        'yaml_filename': mask_yaml_file},
-                        convert_types=True)]
-        ),
-        Node(
-            package='nav2_map_server',
-            executable='costmap_filter_info_server',
-            name='costmap_filter_info_server',
-            namespace=namespace,
-            output='screen',
-            emulate_tty=True,  # https://github.com/ros2/launch/issues/188
-            parameters=[RewrittenYaml(
-                        source_file=params_file,
-                        root_key=namespace,
-                        param_rewrites={'use_sim_time': use_sim_time,
-                                        'yaml_filename': mask_yaml_file},
-                        convert_types=True)]
-        ),
+        # DeclareLaunchArgument(
+        #     'mask',
+        #     default_value = mask_yaml_file,
+        #     description='Full path to filter mask yaml file to load'
+        # ),
+        # Node(
+        #     package='nav2_lifecycle_manager',
+        #     executable='lifecycle_manager',
+        #     name='lifecycle_manager_costmap_filters',
+        #     namespace=namespace,
+        #     output='screen',
+        #     emulate_tty=True,  # https://github.com/ros2/launch/issues/188
+        #     parameters=[{'use_sim_time': use_sim_time},
+        #                 {'autostart': autostart},
+        #                 {'node_names': lifecycle_nodes}]
+        # ),
+        # Node(
+        #     package='nav2_map_server',
+        #     executable='map_server',
+        #     name='filter_mask_server',
+        #     namespace=namespace,
+        #     output='screen',
+        #     emulate_tty=True,  # https://github.com/ros2/launch/issues/188
+        #     parameters=[RewrittenYaml(
+        #                 source_file=params_file,
+        #                 root_key=namespace,
+        #                 param_rewrites={'use_sim_time': use_sim_time,
+        #                                 'yaml_filename': mask_yaml_file},
+        #                 convert_types=True)]
+        # ),
+        # Node(
+        #     package='nav2_map_server',
+        #     executable='costmap_filter_info_server',
+        #     name='costmap_filter_info_server',
+        #     namespace=namespace,
+        #     output='screen',
+        #     emulate_tty=True,  # https://github.com/ros2/launch/issues/188
+        #     parameters=[RewrittenYaml(
+        #                 source_file=params_file,
+        #                 root_key=namespace,
+        #                 param_rewrites={'use_sim_time': use_sim_time,
+        #                                 'yaml_filename': mask_yaml_file},
+        #                 convert_types=True)]
+        # ),
         DeclareLaunchArgument(
             'map',
-            default_value=map_dir,
+            default_value=map_data,
             description='Full path to map file to load'
-        ),          
+        ),    
+        DeclareLaunchArgument(
+            'costmap',
+            default_value=costmap_data,
+            description='Full path to map file to load'
+        ),     
         SetLaunchConfiguration(
             name='params_file',
             value=os.path.join(config_dir, 'params', 'nav2_params.yaml')
@@ -143,7 +149,8 @@ def generate_launch_description():
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource([launch_file_dir, '/bringup_launch.py']),
             launch_arguments={
-                'map': map_dir,
+                'map': map_data,
+                'costmap': costmap_data,
                 'use_sim_time': use_sim_time,
                 'use_composition':'True',
                 'params_file': params_file,
